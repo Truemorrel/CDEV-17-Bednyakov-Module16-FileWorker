c8dcc5de86bf78d65650f14e834ee303dbdd683f
Sergey Bednyakov <sbednyakov@gmail.com>
2022-07-27 01:56:55 +0300

    Add project files.

---
+++ FileWorker.Tests/FileWorkerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FileWorker.Tests
+{
+    [TestClass]
+    public class FileWorkerTests
+    {
+        [TestMethod]
+        public void FileNameMustConsistOfCorrectChars()
+        {
+            string incorrect = @"couple'`/#?words$%,./*/+with,./1234?><=""delimeters";
+            string expected = @"couple'`_#_words$%,.___+with,._1234___=_delimeters";
+            var fileWorker = new FileWorker();
+
+            PrivateObject privateObject = new PrivateObject(fileWorker);
+            Assert.AreEqual(expected, privateObject.Invoke("GetSafeFilename", incorrect));
+        }
+    }
+}
---
+++ FileWorker.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FileWorker.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FileWorker.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e1d1fb07-b9ef-4e73-9d34-f35742393b5f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
